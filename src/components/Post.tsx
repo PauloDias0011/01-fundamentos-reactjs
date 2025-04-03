40d648ea15e7ae19de02b3b3773cbfcba95ab1ee
Paulo Dias <pauloricardo.silvadias@gmail.com>
2025-04-03 10:46:05 -0300

    Estrutura da aplicação - Componente: Comment

--- src/components/Post.tsx
+++ src/components/Post.tsx
@@ -1,4 +1,6 @@
 import styles from './Post.module.css'
+import { Comment } from './Comment';
+
 
 export function Post() {
     return (
@@ -34,6 +36,12 @@ export function Post() {
 
                 </footer>
             </form>
+
+            <div className={styles.commentList}>
+        <Comment />
+        <Comment />
+        <Comment />
+      </div>
         </article>
 
     )
